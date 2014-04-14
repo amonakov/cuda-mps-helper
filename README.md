a5118b0ababf95cec07820ff3ef1af23207f5c76
Alexander Monakov <amonakov@ispras.ru>
2014-04-14 21:36:23 +0400

    Add the script and update README

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 cuda-mps-helper
 ===============
 
-Helper script for running MPI applications with CUDA MPS
+This script can be used to automate starting and stopping CUDA MPS servers on
+multi-GPU nodes, and running MPI processes with MPS dispatch.
+
+To start:
+
+    ./manage-mps.sh start
+
+On a system with both Tesla and GeForce-class cards installed, some GeForce
+cards may be enumerated in CUDA_VISIBLE_DEVICES before Teslas.  In that case,
+run `deviceQuery` CUDA sample to find out the order and pass the device
+numbers as the argument to 'start' command:
+
+    ./manage-mps.sh start 0 2
+
+To signal MPS servers to shut down and remove temporary directories:
+
+    ./manage-mps.sh stop
+
+To run a process via one of MPS server, distributing in a round-robin fashion
+by MPI local rank:
+
+    ./manage-mps.sh wrap *COMMAND*
+
+For instance try
+
+    mpirun -np 4 ./manage-mps.sh wrap bash -c 'echo $CUDA_MPS_PIPE_DIRECTORY'
---
+++ manage-mps.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+MPSBASE="/tmp/nvidia-mps-$USER"
+MPSCONTROL=nvidia-cuda-mps-control
+
+start()
+{
+  if pidof -s "$MPSCONTROL" >/dev/null; then
+    echo "CUDA MPS already running"
+    exit 1
+  fi
+
+  CUDA_DEVICES=$@
+
+  if [ -z "$CUDA_DEVICES" ]; then
+    N_TESLA_DEVICES=$(nvidia-smi -L | grep Tesla | wc -l)
+    CUDA_DEVICES="$(seq 0 $[$N_TESLA_DEVICES - 1])"
+  fi
+
+  MPSDIRS="$(mktemp -d "$MPSBASE-XXXXXX")"
+
+  declare -i j=0
+
+  for i in $CUDA_DEVICES; do
+    D="$MPSDIRS/$j"
+    j=$[$j + 1]
+    mkdir "$D"
+    CUDA_VISIBLE_DEVICES=$i \
+      CUDA_MPS_PIPE_DIRECTORY="$D" \
+      CUDA_MPS_LOG_DIRECTORY="$D" \
+      "$MPSCONTROL" -d
+  done
+}
+
+findmps()
+{
+  local MPS=$(pidof -s "$MPSCONTROL")
+  if [ -z "$MPS" ]; then
+    echo "No MPS running"
+    exit 1
+  fi
+  local MPSPIPEDIR=$(tr '\0' '\n' </proc/$MPS/environ |
+    sed -n 's/^CUDA_MPS_PIPE_DIRECTORY=\(.*\)/\1/p')
+  MPSDIRS=$(dirname $MPSPIPEDIR)
+}
+
+stop()
+{
+  findmps
+  for i in "$MPSDIRS"/*; do
+    CUDA_MPS_PIPE_DIRECTORY="$i" "$MPSCONTROL" <<<quit
+  done
+  rm -rf "$MPSDIRS"
+}
+
+wrap()
+{
+  findmps
+  N=$(ls "$MPSDIRS/" | wc -l)
+  I=${MV2_COMM_WORLD_LOCAL_RANK-${OMPI_COMM_WORLD_LOCAL_RANK-0}}
+  export CUDA_MPS_PIPE_DIRECTORY="$MPSDIRS/$[$I % $N]"
+  exec "$@"
+}
+
+command="$1"
+shift
+
+case "$command" in
+  "start" | "stop" | "wrap")
+    "$command" "$@"
+    ;;
+  *)
+    echo "usage: $0 {start [DEVICE...] | stop | wrap COMMAND}"
+esac
