6765a8169238dd437aa7d1e929b3567f67f86f64
Alexander Monakov <amonakov@ispras.ru>
2014-07-08 13:43:06 +0400

    Use lock on /tmp to serialize star/stop

--- manage-mps.sh
+++ manage-mps.sh
@@ -3,11 +3,10 @@
 : ${MPSBASE:="/tmp/nvidia-mps-$USER"}
 : ${MPSCONTROL:=nvidia-cuda-mps-control}
 
-start()
+start_()
 {
   if pgrep -u "$UID" -fx "$MPSCONTROL -d" >/dev/null; then
-    echo "CUDA MPS already running"
-    exit 1
+    exit
   fi
 
   CUDA_DEVICES=$@
@@ -30,43 +29,69 @@ start()
       CUDA_MPS_LOG_DIRECTORY="$D" \
       "$MPSCONTROL" -d
   done
+
+  echo "$MPSDIRS"
 }
 
-findmps()
+start()
+{
+  MPSDIRS="$(flock -o /tmp -c "$0 start_ $@")"
+  if [ -n "$MPSDIRS" ]; then
+    echo MPSDIRS="$MPSDIRS"
+  fi
+}
+
+lookup()
 {
   local MPS=$(pgrep -u "$UID" -fx "$MPSCONTROL -d")
   if [ -z "$MPS" ]; then
-    echo "No MPS running"
-    exit 1
+    return
   fi
   local MPSPIPEDIR=$(tr '\0' '\n' </proc/$MPS/environ |
     sed -n 's/^CUDA_MPS_PIPE_DIRECTORY=\(.*\)/\1/p')
-  MPSDIRS=$(dirname $MPSPIPEDIR)
+  if [ -n "$MPSPIPEDIR" ]; then
+    dirname "$MPSPIPEDIR"
+  else
+    echo /tmp/nvidia-mps
+  fi
 }
 
-stop()
+stop_()
 {
-  findmps
+  MPSDIRS="$(lookup)"
   for i in "$MPSDIRS"/*; do
     CUDA_MPS_PIPE_DIRECTORY="$i" "$MPSCONTROL" <<<quit
   done
   rm -rf "$MPSDIRS"
 }
 
-wrap()
+stop()
 {
-  findmps
+  flock -o /tmp -c "$0 stop_"
+}
+
+wrap_()
+{
+  local MPSDIRS="$(lookup)"
   N=$(ls "$MPSDIRS/" | wc -l)
   I=${MV2_COMM_WORLD_LOCAL_RANK-${OMPI_COMM_WORLD_LOCAL_RANK-0}}
-  export CUDA_MPS_PIPE_DIRECTORY="$MPSDIRS/$[$I % $N]"
-  exec "$@"
+  CUDA_MPS_PIPE_DIRECTORY="$MPSDIRS/$[$I % $N]" "$@"
+}
+
+wrap()
+{
+  start >/dev/null
+  wrap_ "$@"
+  if [ -n "$MPSDIRS" ]; then
+    stop
+  fi
 }
 
 command="$1"
 shift
 
 case "$command" in
-  "start" | "stop" | "wrap")
+  start | start_ | stop | stop_ | wrap | wrap_)
     "$command" "$@"
     ;;
   *)
